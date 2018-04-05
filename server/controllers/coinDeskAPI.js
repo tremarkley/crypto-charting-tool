6fb623e3b40231c0ba461447e3b52822d903bc43
Harry Markley <tremarkley@gmail.com>
2018-04-05 00:16:54 -0700

    Clean up comments

--- server/controllers/coinDeskAPI.js
+++ server/controllers/coinDeskAPI.js
@@ -19,14 +19,3 @@ const getPricesBetweenDates = async ({ start, end }) => {
 module.exports = {
   getPricesBetweenDates,
 };
-
-//  get symbol id that you are interested in
-//  make GET @ https://api.coindesk.com/v1/bpi/historical/close.json?
-//  start: 2016-01-15
-//  end: `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`
-
-//  data shape: {"bpi":
-//  {"2018-02-04":8186.6488,"2018-02-05":6914.26,"2018-02-06":7700.3863,"2018-02-07":7581.8038},
-//  "disclaimer":"This data was produced from the CoinDesk...",
-//  "time":{"updated":"Feb 11, 2018 00:03:00 UTC",
-//  "updatedISO":"2018-02-11T00:03:00+00:00"}}
